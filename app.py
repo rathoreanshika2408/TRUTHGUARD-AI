564e3fae832a3519a601a7ade8c175804a7fd97c
himanshu <himanshu@himanshus-MacBook-Air-2.local>
2026-03-17 03:41:14 +0530

    Open CORS for testing

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from twilio.rest import Client
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, origins=["https://trustguardai-rose.vercel.app", "http://127.0.0.1:5500", "http://localhost:5500"])
+CORS(app)
 groq_client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 twilio_client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
 
